c7789ee91245bc9feffee52d1c19e7b658f9acf4
moxizhou <mzhou8@u.rochester.edu>
2014-09-11 22:25:30 -0700

    Almost done with tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,38 @@
-var makeTree = function(value){
-  var newTree = {};
-  newTree.value = value;
-  newTree.children = undefined;
-  return newTree;
+var extend= function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  this.children.push(makeTree(value));
 
 };
 
 treeMethods.contains = function(target){
 
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++){
+      if(this.contains(this.children[i])) {
+        return this.contains(this.children[i]);
+      }
+    }
+  }
+  return false;
+};
+
+
+var makeTree = function(value){
+  var newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+  extend(newTree, treeMethods);
+  return newTree;
 };
 
 
